bc7018171b48564453fcfde5ce4d7a0fffcef9aa
joaopedroluz57 <joao.pedro.luz@hotmail.com>
2022-02-24 11:25:49 -0300

    Implementando autorizacao

--- src/main/java/com/joaopedroluz/cursomc/security/JWTUtil.java
+++ src/main/java/com/joaopedroluz/cursomc/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.joaopedroluz.cursomc.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -17,9 +18,9 @@ public class JWTUtil {
     private String secret;
     @Value("${jwt.expiration}")
     private Long expiration;
-    
 
-    public String generateToken(String email){
+
+    public String generateToken(String email) {
         SecretKey key = Keys.hmacShaKeyFor(Objects.requireNonNull(secret).getBytes(StandardCharsets.UTF_8));
 
         return Jwts.builder()
@@ -27,5 +28,32 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(key)
                 .compact();
+    }
+
+    public boolean tokenValido(String token) {
+        Claims claims = getClaims(token);
+        if (claims != null) {
+            String username = claims.getSubject();
+            Date expirationDate = claims.getExpiration();
+            Date now = new Date(System.currentTimeMillis());
+            return username != null && expirationDate != null && now.before(expirationDate);
+        }
+        return false;
+    }
+
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        if (claims != null) {
+            return claims.getSubject();
+        }
+        return null;
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(secret.getBytes(StandardCharsets.UTF_8)).build().parseClaimsJws(token).getBody();
+        } catch (Exception err) {
+            return null;
+        }
     }
 }
