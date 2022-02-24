a674f6e306295f30192ee2cce96276d555f4cc9e
joaopedroluz57 <joao.pedro.luz@hotmail.com>
2022-02-24 09:39:57 -0300

    Implementando autenticacao e egracao do token JWT

---
+++ src/main/java/com/joaopedroluz/cursomc/security/JWTUtil.java
@@ -0,0 +1,31 @@
+package com.joaopedroluz.cursomc.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Objects;
+
+@Component
+public class JWTUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Long expiration;
+    
+
+    public String generateToken(String email){
+        SecretKey key = Keys.hmacShaKeyFor(Objects.requireNonNull(secret).getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+}
