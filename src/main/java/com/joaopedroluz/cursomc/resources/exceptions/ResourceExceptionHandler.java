8f2076d64e537aa196e342a9f3a4b6e96b8394f9
joaopedroluz57 <joao.pedro.luz@hotmail.com>
2022-02-25 13:42:48 -0300

    Tratando excecoes da Amazon

--- src/main/java/com/joaopedroluz/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/joaopedroluz/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.joaopedroluz.cursomc.resources.exceptions;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.joaopedroluz.cursomc.services.exceptions.AuthorizationException;
 import com.joaopedroluz.cursomc.services.exceptions.DataIntegrityViolationException;
+import com.joaopedroluz.cursomc.services.exceptions.FileException;
 import com.joaopedroluz.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,5 +43,30 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
         StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+    }
+
+    @ExceptionHandler(FileException.class)
+    public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonServiceException.class)
+    public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
+        HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+        StandardError err = new StandardError(System.currentTimeMillis(), code.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(code).body(err);
+    }
+
+    @ExceptionHandler(AmazonClientException.class)
+    public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonS3Exception.class)
+    public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, HttpServletRequest request) {
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
---
+++ src/main/java/com/joaopedroluz/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,11 @@
+package com.joaopedroluz.cursomc.services.exceptions;
+
+public class FileException extends RuntimeException{
+    public FileException(String msg){
+        super(msg);
+    }
+
+    public FileException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+}
