0ac79fbbc785f19d66e2a6d98f875bcc25265943
joaopedroluz57 <joao.pedro.luz@hotmail.com>
2022-02-27 21:12:43 -0300

    Fix: List<FieldMessage> getErrors()

--- src/main/java/com/joaopedroluz/cursomc/resources/exceptions/ValidationError.java
+++ src/main/java/com/joaopedroluz/cursomc/resources/exceptions/ValidationError.java
@@ -11,7 +11,7 @@ public class ValidationError extends StandardError{
         super(timeStamp, status, error, message, path);
     }
 
-    public List<FieldMessage> getErros() {
+    public List<FieldMessage> getErrors() {
         return errors;
     }
 
